e393f7cad651b08fec35e9f26f766f794387a744
nxhuy1697 <nxhuy1697@gmail.com>
2024-10-28 19:12:41 +0700

    update

--- src/components/Blub.tsx
+++ src/components/Blub.tsx
@@ -3,8 +3,10 @@ import Image from "next/image";
 const Blub = () => {
   return (
     <div className="absolute -left-36 -bottom-12 rotate-12 mix-blend-color-dodge animate-pulse duration-75 z-10 w-[200px] xl:w-[260px]">
+      {/* img  */}
       <Image
         src={"/bulb.png"}
+        
         width={260}
         height={200}
         className="w-full h-full"
