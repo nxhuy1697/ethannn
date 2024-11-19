9c1d6bbaa6e86f7636f0e3ab023a89a66b6a75fb
nxhuy1697 <nxhuy1697@gmail.com>
2024-11-19 16:40:07 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="hidden xl:flex absolute bottom-0 -left-[150px]"
+            className="hidden xl:flex absolute bottom-0 -left-[100px]"
           >
             <AvatarD />
           </motion.div>
