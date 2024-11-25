4cea2590bdbfb9241de4834e4c1748401d034b62
nxhuy1697 <nxhuy1697@gmail.com>
2024-11-25 11:21:41 +0700

    validate form

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -69,7 +69,7 @@ const About = () => {
         initial="hidden"
         animate="show"
         exit="hidden"
-        className="hidden xl:flex absolute bottom-0 -left-[270px]"
+        className="hidden xl:flex absolute bottom-0 -left-[370px]"
       >
         <Avatar />
       </motion.div>
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,7 +6,6 @@ import { fadeIn } from "../../../variant";
 
 // react-hook-form
 import { SubmitHandler, useForm } from "react-hook-form";
-
 // icons
 import { BsArrowRight } from "react-icons/bs";
 //translation
@@ -14,11 +13,11 @@ import "@/utils/i18n";
 import { useTranslation } from "react-i18next";
 
 //types
-type FormFileds = {
+type FormFields = {
   name: string;
   email: string;
   subject: string;
-  mess: string;
+  message: string;
 };
 
 const Contact = () => {
@@ -27,12 +26,12 @@ const Contact = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormFileds>();
+  } = useForm<FormFields>();
 
-  const onSubmit: SubmitHandler<FormFileds> = (data) => {
-    console.log("Form Data: ", data);
+  const onSubmit: SubmitHandler<FormFields> = (data) => {
+    console.log("Formdata: ", data);
+    console.log("first");
   };
-
   return (
     <div className="h-full bg-primary/30">
       <div className="container mx-auto pt-[50px] text-center xl:text-left flex items-center justify-center h-full">
@@ -70,33 +69,54 @@ const Contact = () => {
             className="flex-1 flex flex-col gap-6 w-full mx-auto "
           >
             {/* input group  */}
-            <div className="flex gap-x-6 w-full">
+            <div className="flex gap-x-6">
+              <div className="w-full">
               <input
                 type="text"
                 placeholder={t("contact.form_name")}
-                {...register("name", { required: true })}
+                {...register("name", { required: t('contact.vali_name') })}
                 className="input border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-accent focus:border-transparent"
               />
-
+              {errors.name && (
+                <div className="text-red-500 capitalize">{errors.name?.message} </div>
+              ) }
+              </div>
+              <div className="w-full">
               <input
                 type="email"
                 placeholder="email"
-                {...register("email", { required: true,  })}
+                {...register("email", {
+                  required: t('contact.vali_emptyEmail'),
+                  pattern:{
+                    value:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                    message: t('contact.vali_invalidEmail')
+                  } 
+                })}
                 className="input border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-accent focus:border-transparent"
               />
+               {errors.email && (
+                <div className="text-red-500 capitalize">{errors.email?.message} </div>
+              )}
+
+              </div>
             </div>
             <input
               type="text"
               placeholder={t("contact.form_subject")}
-              required
+              {...register("subject", { required: t('contact.vali_subject') })}
               className="input border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-accent focus:border-transparent"
             />
-
+               {errors.subject && (
+                <div className="text-red-500 capitalize">{errors.subject?.message} </div>
+              )}
             <textarea
               placeholder={t("contact.form_mess")}
-              required
+              {...register("message", { required: t('contact.vali_message') })}
               className="textarea border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-accent focus:border-transparent"
             ></textarea>
+             {errors.message && (
+                <div className="text-red-500 capitalize">{errors.message?.message} </div>
+              )}
 
             <button className="btn rounded-full border border-white max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group ">
               <span className="group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500 ">
